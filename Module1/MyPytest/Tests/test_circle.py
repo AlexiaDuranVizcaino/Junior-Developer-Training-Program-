3407f0ce53b985db5aa7fcf3732d844abc35e46c
Alexia <alexia.duran@Sartorius.com>
2025-09-23 16:54:16 +0200

    more

--- Module1/MyPytest/Tests/test_circle.py
+++ Module1/MyPytest/Tests/test_circle.py
@@ -3,5 +3,11 @@ from Source import shapes as shapes
 
 class TestCircle:
 
+    def setup_method(self, method):
+        print(f"Setting up {method}")
+
+    def teardown_method(self, method):
+        print(f"Tearing down {method}")
+        
     def test_one(self):
         assert True
